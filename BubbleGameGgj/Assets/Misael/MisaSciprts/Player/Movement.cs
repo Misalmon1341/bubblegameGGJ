8c53ac0f9b6b74d0d5ec17df5e51ed456f67298c
Kary Vellenaweth <misagamer559@gmail.com>
2025-01-25 15:42:02 -0600

    Movimiento y animaciones  terminadas

--- BubbleGameGgj/Assets/Misael/MisaSciprts/Player/Movement.cs
+++ BubbleGameGgj/Assets/Misael/MisaSciprts/Player/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -8,16 +9,41 @@ public class Movement : MonoBehaviour
     [SerializeField] private Vector2 direccion;
 
     private Rigidbody2D rb2D;
-    // Start is called before the first frame update
+
+    private float movimientoX;
+    private float movimientoY;
+    private Animator animator;
+    [SerializeField] private ParticleSystem particulas;
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        movimientoX = Input.GetAxisRaw("Horizontal");
+        movimientoY = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("MovimientoX", movimientoX);
+        animator.SetFloat("MovimientoY", movimientoY);
+        if (movimientoX != 0 || movimientoY != 0)
+        {
+            animator.SetFloat("UltimoX", movimientoX);
+            animator.SetFloat("UltimoY", movimientoY);
+        }
+        direccion = new Vector2(movimientoX, movimientoY).normalized;
     }
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Mueble"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                particulas.Play();
+            }
+        }
+    }*/
     private void FixedUpdate()
     {
         rb2D.MovePosition(rb2D.position + direccion * velocidadMovimiento * Time.fixedDeltaTime);
